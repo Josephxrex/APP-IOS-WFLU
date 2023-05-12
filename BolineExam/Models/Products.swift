143e5142ca169f705af0815710c9fc1ca43b1455
Joseph Camargo <jc72705@udelasalle.edu.mx>
2023-05-12 17:37:48 -0600

    Add Models

--- BolineExam/Models/Products.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Products.swift
-//  BolineExam
-//
-//  Created by ISSC_612_2023 on 12/05/23.
-//
-
-import Foundation
--- BolineExam/Models/Sales.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Sales.swift
-//  BolineExam
-//
-//  Created by ISSC_612_2023 on 12/05/23.
-//
-
-import Foundation
