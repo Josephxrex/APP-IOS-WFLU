9e7993f7f16215834e9a2494289999a5b9bfa637
Joseph Camargo <jc72705@udelasalle.edu.mx>
2023-05-12 16:43:07 -0600

    Initial Commit

---
+++ BolineExam/BolineExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  BolineExamApp.swift
+//  BolineExam
+//
+//  Created by ISSC_612_2023 on 12/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BolineExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
