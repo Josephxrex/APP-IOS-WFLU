bae95f71092c36c33fa0f9f6af5984191290a051
Axel <axelglez1@hotmail.com>
2023-05-19 13:32:49 -0600

    Menu completo

--- BolineExam/View/Menu.swift
+++ BolineExam/View/Menu.swift
@@ -21,23 +21,29 @@ struct Menu: View {
         let id = UUID()
         let title: String
         let iconName: String
+        let destination: AnyView
     }
     
     let items = [
-            Item(title: "Elemento 1", iconName: "icono1"),
-            Item(title: "Elemento 2", iconName: "icono2"),
-            Item(title: "Elemento 3", iconName: "icono3")
+        Item(title: "Users", iconName: "person.fill", destination: AnyView(UsersListView())),
+        Item(title: "Purchases", iconName: "cart.fill", destination: AnyView(PurchasesListView())),
+
+        Item(title: "Sales", iconName: "tag.fill", destination: AnyView(SalesListView())),
+        Item(title: "Products", iconName: "bag.fill", destination: AnyView(ProductsListView())),
+             
         ]
     
+
     var body: some View {
+
+        
         Text("Welcome back!")
             .font(.largeTitle.bold()).padding(.top)
             .frame(maxWidth: .infinity, alignment: .leading).padding(.top)
         Text("Pick up where you left off").frame(maxWidth: .infinity, alignment: .leading)
         
-        NavigationView {
                     List(items) { item in
-                        NavigationLink(destination: Text(item.title)) {
+                        NavigationLink(destination: item.destination.edgesIgnoringSafeArea(.all)) {
                             HStack {
                                 Image(systemName: item.iconName)
                                     .foregroundColor(.blue)
@@ -46,7 +52,8 @@ struct Menu: View {
                             }
                         }
                     }
-        }.padding()
         }
     }
+
+
     
